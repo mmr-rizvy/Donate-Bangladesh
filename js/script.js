0521d119c532566001eb2bf5f809c5ad0336bf08
Md. Musfhikur Rahman Rizvy <rizvynews24@gmail.com>
2024-09-24 10:36:13 +0600

    History Button Functionality Done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// Section Showing & Hiding Functionality Start Here
+const showSection = function (idName) {
+    document.getElementById('donation-container').classList.add('hidden');
+    document.getElementById('transaction-history').classList.add('hidden');
+    document.getElementById(idName).classList.remove('hidden');
+}
+
+
 // Donate Functionality Start Here
 const donateProcess = function (amountForDonate, mainBalance, balanceInAccount) {
     const donationAmount = Number(document.getElementById(amountForDonate).value);
@@ -10,6 +18,13 @@ const donateProcess = function (amountForDonate, mainBalance, balanceInAccount) 
         const newMainBalance = accountBalance -= donationAmount;
         document.getElementById('account-balance').innerText = newMainBalance;
         document.getElementById('my_modal_5').showModal()
+        // History
+        const donateFor = document.querySelector('.donate-for').innerText;
+        const history = document.createElement('history');
+        const dateTime = new Date();
+        history.innerHTML = `<div class="p-4 border rounded-xl mb-4"><h3 class="text-lg font-bold mb-2">${donationAmount} Taka Is Donated For ${donateFor}</h3>
+        <p>Date: ${dateTime}</p></div>`;
+        document.getElementById('transaction-history').appendChild(history);
     } else {
         alert('Invalid Donation Amount')
     }
@@ -24,4 +39,12 @@ document.getElementById('btn-donate-2').addEventListener('click', function () {
 })
 document.getElementById('btn-donate-3').addEventListener('click', function () {
     donateProcess('donation-amount-3', 'main-balance-3', 'account-balance');
+})
+
+// Section Showing & Hiding Functionality End Here
+document.getElementById('btn-donation').addEventListener('click', function () {
+    showSection('donation-container');
+})
+document.getElementById('btn-history').addEventListener('click', function () {
+    showSection('transaction-history');
 })
