c29267dc675640fe2ede9e7e00d27e9ef12b6581
Md. Musfhikur Rahman Rizvy <rizvynews24@gmail.com>
2024-09-24 14:22:51 +0600

    The App is Now Fully FUnctional

--- js/script.js
+++ js/script.js
@@ -5,32 +5,45 @@ const showSection = function (idName) {
     document.getElementById(idName).classList.remove('hidden');
 }
 
-
 // Donate Functionality Start Here
 const donateProcess = function (amountForDonate, mainBalance, balanceInAccount) {
     const donationAmount = Number(document.getElementById(amountForDonate).value);
-    console.log(donationAmount);
     let totalDonation = Number(document.getElementById(mainBalance).innerText);
     let accountBalance = Number(document.getElementById(balanceInAccount).innerText);
+    // Here is the Validation Process
     if (typeof (donationAmount) === 'number' && !isNaN(donationAmount) && donationAmount > 0 && donationAmount <= accountBalance) {
         const newTotal = totalDonation += donationAmount;
         document.getElementById(mainBalance).innerText = newTotal;
         const newMainBalance = accountBalance -= donationAmount;
         document.getElementById('account-balance').innerText = newMainBalance;
         document.getElementById('my_modal_5').showModal()
-        // History
-        const donateFor = document.querySelector('.donate-for').innerText;
-        const history = document.createElement('history');
-        const dateTime = new Date();
-        history.innerHTML = `<div class="p-4 border rounded-xl mb-4"><h3 class="text-lg font-bold mb-2">${donationAmount} Taka Is Donated For ${donateFor}</h3>
-        <p>Date: ${dateTime}</p></div>`;
-        document.getElementById('transaction-history').appendChild(history);
+        // History Section is Here
+        const DonateForNoakhali = document.getElementById('noakhali').innerText;
+        const DonateForFeni = document.getElementById('feni').innerText;
+        const DonateForQuota = document.getElementById('quota').innerText;
+
+        // A Common Function For Decide Donation Purpose
+        const donationPurpose = function (donateFor) {
+            const history = document.createElement('history');
+            const dateTime = new Date();
+            history.innerHTML = `<div class="p-4 border rounded-xl mb-4"><h3 class="text-lg font-bold mb-2">${donationAmount} Taka Is Donated For ${donateFor}</h3>
+    <p>Date: ${dateTime}</p></div>`;
+            document.getElementById('transaction-history').appendChild(history);
+        }
+        // Donation Purpose
+        if (mainBalance === 'main-balance-1') {
+            donationPurpose(DonateForNoakhali);
+        } else if (mainBalance === 'main-balance-2') {
+            donationPurpose(DonateForFeni);
+        } else if (mainBalance === 'main-balance-3') {
+            donationPurpose(DonateForQuota);
+        }
+
     } else {
         alert('Invalid Donation Amount')
     }
 }
 // Donate Functionality End Here
-
 document.getElementById('btn-donate-1').addEventListener('click', function () {
     donateProcess('donation-amount-1', 'main-balance-1', 'account-balance');
 })
